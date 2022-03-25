697b0121a4fa669e54a3c2a8b126f5b79f72f16f
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2022-03-25 15:02:37 -0500

    Update main.tf

--- routing/main.tf
+++ routing/main.tf
@@ -8,12 +8,22 @@ resource "ibm_is_subnet_routing_table_attachment" "vm2_subnet" {
   routing_table = var.vm2_subnet_routing_table
 }
 
-resource "ibm_is_vpc_routing_table_route" "vm1_subnet_route" {
+resource "ibm_is_vpc_routing_table_route" "vm1_subnet_route_table1" {
   vpc           = var.vpc_id
   routing_table = var.vm1_subnet_routing_table
   zone          = var.zone
-  name          = "${var.name}-vm1-subnet-route"
+  name          = "${var.name}-vm1-subnet-route-table-1"
   destination   = var.vm1_subnet_cidr
+  action        = "deliver"
+  next_hop      = var.fortiate_port_1_private_ip
+}
+
+resource "ibm_is_vpc_routing_table_route" "vm2_subnet_route_table1" {
+  vpc           = var.vpc_id
+  routing_table = var.vm1_subnet_routing_table
+  zone          = var.zone
+  name          = "${var.name}-vm2-subnet-route-table-1"
+  destination   = var.vm2_subnet_cidr
   action        = "deliver"
   next_hop      = var.fortiate_port_1_private_ip
 }
@@ -22,7 +32,7 @@ resource "ibm_is_vpc_routing_table_route" "vm2_subnet_route" {
   vpc           = var.vpc_id
   routing_table = var.vm2_subnet_routing_table
   zone          = var.zone
-  name          = "${var.name}-vm2-subnet-route"
+  name          = "${var.name}-vm2-subnet-route-table-2"
   destination   = var.vm2_subnet_cidr
   action        = "deliver"
   next_hop      = var.fortiate_port_1_private_ip
